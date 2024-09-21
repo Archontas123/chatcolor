fd617bf748456813c5acea7c814ed33b2ed621d4
Archontas123 <tavuc123@hotmail.com>
2024-09-21 21:06:41 +0400

    Added code

---
+++ src/main/java/org/mythofy/chatColor/ChatColorsPlugin.java
@@ -0,0 +1,17 @@
+package org.mythofy.chatColor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatColorsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
