7266a702b8a755bcb4a836095d5e885b562ba563
Archontas123 <tavuc123@hotmail.com>
2024-09-22 13:43:55 -0400

    Delete src/main directory

--- src/main/java/org/mythofy/chatcolors/ColorUtil.java
+++
@@ -1,9 +0,0 @@
-package org.mythofy.chatcolors;
-
-import java.awt.Color;
-
-public class ColorUtil {
-    public static int hsvToRgb(float hue, float saturation, float value) {
-        return Color.HSBtoRGB(hue / 360.0F, saturation, value) & 0xFFFFFF;
-    }
-}
